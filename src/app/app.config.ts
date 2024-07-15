addccc533e3d4c30878c5ea1b3a4c612b975d726
VikasniArun <143959987+VikasniArun@users.noreply.github.com>
2024-07-15 10:32:59 +0530

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,7 @@ import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +20,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-c91d9","appId":"1:203311750375:web:73b62e53e3a7c295461642","storageBucket":"friendlychat-c91d9.appspot.com","locationId":"asia-south1","apiKey":"AIzaSyBZcYrL7-ydwDjkp4NmZbyz7_w9nrh_Vm0","authDomain":"friendlychat-c91d9.firebaseapp.com","messagingSenderId":"203311750375"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideMessaging(() => getMessaging())), importProvidersFrom(providePerformance(() => getPerformance())), importProvidersFrom(provideStorage(() => getStorage()))
   ],
 };
